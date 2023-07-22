336d9a9432dc1949e8e2ebc27ad81598bea3d9ee
Kamal Muradov <muradovkamal10@gmail.com>
2023-07-22 00:37:43 -0400

    implement

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # clipboard-to-url
-A CLI tool that uploads images or videos from your clipboard to Google Cloud Storage and provides a shareable URL
+
+A CLI tool that uploads images, videos, or files from your clipboard to Google Cloud Storage and provides a shareable URL
+
+![mp4 path](./assets/mp4%20path.mp4)
+
+## Usage
+
+(1) Copy an image, path to an image, or path to a video
+
+(2) Run `python clipboard_to_url.py`
+
+(3) Paste the URL
+
+## Setup
+
+(1) Create a bucket https://cloud.google.com/storage/docs/creating-buckets
+
+(2) Make the bucket public by granting the `Storage Object Viewer` role to the `allUsers` principal (see [this](https://cloud.google.com/storage/docs/access-control/making-data-public) guide for more details). This allows anyone on the internet to _view_ files that are in this bucket, but only you can upload files to this bucket. When all is said and done, it should look like this:
+
+![bucket_permissions](./assets//bucket_permissions.png)
+
+(3) Copy `.env.example` to `.env` and fill out the values
+
+(4) Create a python virtual environment `python -m venv env`
+
+(5) Install dependencies `pip install wheel && pip install -r requirements.txt`
---
+++ clipboard_to_url.py
@@ -0,0 +1,119 @@
+import sys
+import pyperclip
+import hashlib
+import io
+import mimetypes
+
+from PIL import Image, UnidentifiedImageError
+from PIL.Image import Image as PILImage
+from PIL import ImageGrab
+from pathlib import Path
+from dotenv import dotenv_values
+from google.cloud import storage
+
+
+def extension_to_type(extension: str) -> str:
+    assert isinstance(extension, str), f"Expected extension to be str, received: {extension}"
+    content_type = mimetypes.types_map.get(extension)
+    assert content_type, f"Failed to get content_type for {extension=}"
+    return content_type
+
+
+def image_to_bytes(im: PILImage) -> bytes:
+    im_prep = im.convert('RGB')
+    with io.BytesIO() as buffer:
+        im_prep.save(buffer, format="JPEG", jpeg_quality=jpeg_quality)
+        return buffer.getvalue()
+
+
+def hash_bytes(value: bytes) -> str:
+    hasher = hashlib.md5()
+    hasher.update(value)
+    return hasher.hexdigest()
+
+
+def prepare_image(image: PILImage) -> tuple[bytes, str]:
+    content = image_to_bytes(image)
+    content_hash = hash_bytes(content)
+    blob_name = f"{content_hash}.jpg"
+    return content, blob_name
+
+
+def read_from_path(path: Path) -> tuple[bytes, str]:
+    assert path.exists() and path.is_file()
+
+    try:
+        with Image.open(path, formats=["JPEG", "PNG"]) as image:
+            return prepare_image(image)
+    except UnidentifiedImageError:
+        pass
+
+    content = path.read_bytes()
+    content_hash = hash_bytes(content)
+    blob_name = f"{content_hash}{path.suffix}"
+    return content, blob_name
+
+
+def upload_blob(content, blob_name):
+    client = storage.Client(project=project_id)
+    bucket = client.bucket(bucket_id)
+    blob = bucket.blob(blob_name)
+    content_type = extension_to_type(Path(blob_name).suffix)
+    blob.upload_from_string(content, content_type=content_type)
+    return blob.public_url
+
+
+if __name__ == "__main__":
+    env_file = Path(__file__).parent.joinpath(".env").resolve()
+    if not env_file.exists():
+        print(f"Missing file {env_file}")
+        sys.exit()
+
+    config = dotenv_values(env_file)
+
+    project_id = config.get("PROJECT_ID")
+    if not project_id:
+        print(f"Missing PROJECT_ID in {env_file}")
+        sys.exit()
+
+    bucket_id = config.get("BUCKET_ID")
+    if not bucket_id:
+        print(f"Missing BUCKET_ID in {env_file}")
+        sys.exit()
+
+    jpeg_quality = config.get("JPEG_QUALITY")
+    if not jpeg_quality:
+        print(f"Missing JPEG_QUALITY in {env_file}")
+        sys.exit()
+    try:
+        jpeg_quality = int(jpeg_quality)
+    except ValueError:
+        print(f"Expected JPEG_QUALITY to be an integer, received {jpeg_quality}")
+        sys.exit()
+
+    image: PILImage | None = None
+    try:
+        value = ImageGrab.grabclipboard()
+        if isinstance(value, PILImage):
+            image = value
+    except Exception as e:
+        pass
+
+    content: bytes | None = None
+    blob_name: str | None = None
+
+    if image is not None:
+        content, blob_name = prepare_image(image)
+    
+    if content is None and (value := pyperclip.paste()):        
+        try:
+            content, blob_name = read_from_path(Path(value))
+        except Exception:
+            pass
+    
+    if content is None:
+        print("Nothing to upload")
+        sys.exit()
+
+    url = upload_blob(content, blob_name)
+    pyperclip.copy(url)
